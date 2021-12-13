593b12b0b31bad25fe178643cab7cf2b27c38e89
Alexandr Farafonov <a.farafonov@strikerz.inc>
2021-12-13 19:34:35 +0300

    Added example project

---
+++ Source/NetworkResearch/NetworkResearchGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkResearchGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANetworkResearchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetworkResearchGameMode();
+};
+
+
+
